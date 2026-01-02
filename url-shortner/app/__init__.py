7d46dd29add3442bb475f2640eacc25f31cc2dd6
YouCanCallMeRoot <140123417+RootGeek@users.noreply.github.com>
2026-01-02 07:36:06 +0100

    Delete url-shortner directory

--- url-shortner/app/__init__.py
+++
@@ -1,1 +0,0 @@
-# RootGeek application package
--- url-shortner/app/config.py
+++
@@ -1,9 +0,0 @@
-import os
-
-class Settings:
-    PROJECT_NAME: str = "RootGeek"
-    SECRET_KEY: str = os.getenv("ROOTGEEK_SECRET_KEY", "change-this-secret-in-production")
-    SESSION_COOKIE_NAME: str = "linkscope_session"
-    DATABASE_URL: str = os.getenv("ROOTGEEK_DATABASE_URL", "sqlite:///./linkscope.db")
-
-settings = Settings()
